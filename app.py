d4c77983c6162d2e9ff668d54d2200224fcf4600
jaligi navaneetha <jaliginavya269@gmail.com>
2026-09-07 17:29:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -298,7 +298,7 @@ Question:
 
         try:
             response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.6-flash",
             contents=prompt
         )
         except Exception as e:
